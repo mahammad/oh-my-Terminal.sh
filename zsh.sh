0fedf9c122c5327efb302bcd841db982f34ab32d
Mehemmed <msxiyev@bELL.(none)>
2014-05-08 18:11:39 +0300

    zsh sell installing

---
+++ zsh.sh
@@ -0,0 +1,7 @@
+wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+sudo apt-get install zsh
+cp ~/.zshrc ~/.zshrc.orig
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+chsh -s /bin/zsh
+sudo init 6
