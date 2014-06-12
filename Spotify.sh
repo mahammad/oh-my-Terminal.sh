cd261f9d3b47346a5559eca90a5180ba2f929aae
Mehemmed <msxiyev@bELL.(none)>
2014-06-12 17:16:39 +0300

    updated Spotify

--- Spotify.sh
+++ Spotify.sh
@@ -1,5 +1,6 @@
 # /etc/apt/sources.list deb http://repository.spotify.com stable non-free bölümünü düzenleyerek
 #havuz listene ekle
+sudo add-apt-repository "deb http://repository.spotify.com stable non-free"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59 
 sudo apt-get update
 sudo apt-get install spotify-client
