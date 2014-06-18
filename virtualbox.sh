4144839dadbd1147a07cd814ef6b0dc2a7607b68
msxiyev <msxiyev@gmail.com>
2014-06-18 17:22:31 +0300

    added virtualbox.sh

---
+++ virtualbox.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+apt-cache search virtualbox
+apt-cache show virtualbox
+sudo apt-get install virtualbox -y || -e
+echo
+echo "\n\n\tGithub'da STAR icin Teşekkürlerimi sunarım...\n\t\t\tMehemmed (:)"
+
+
