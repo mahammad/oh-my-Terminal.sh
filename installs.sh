a71cbb1c178fbb3095ca2fdfacc3cb7d1cb0b7fa
msxiyev <msxiyev@gmail.com>
2014-06-18 17:25:42 +0300

    updated install.sh

--- installs.sh
+++ installs.sh
@@ -17,4 +17,5 @@ sudo apt-get install xmms -y || -e
 sudo apt-get install curl -y || -e
 sudo apt-get install libavcodec-extra-53 -y || -e #youtube-dl ERROR: audio conversion failed: Unknown encoder 'libmp3lame
 sudo apt-get install yum -y || -e
+sudo apt-get update
 echo "\n\n\n\n\tGithub'da STAR icin Teşekkürlerimi sunarım...\n\t\t\tMehemmed (:)"
