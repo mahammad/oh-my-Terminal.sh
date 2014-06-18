72cd61b71ac5764d35333591590e8bae9e34dc58
msxiyev <msxiyev@gmail.com>
2014-06-18 17:15:07 +0300

    added mpsyt

---
+++ mpsyt.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python-pip -y || -e
+#Then install mps-youtube using pip:
+sudo pip install mps-youtube -y || -e
+sudo apt-get install mplayer -y || -e
+#To run mps-youtube, use the following command:
+#To run
+mpsyt
+#To search for a video on YouTube, prefix your search with either # . or /
