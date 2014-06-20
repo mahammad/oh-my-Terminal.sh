405b02094bf7e60b2e59f0b29364c189d3532c01
msxiyev <msxiyev@gmail.com>
2014-06-20 14:22:32 +0300

    updated update-clean.sh

--- update-clean.sh
+++ update-clean.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/
+
 sudo apt-get update
 sudo apt-get upgrade -y || -e
 sudo apt-get autoremove -y || -e
 sudo apt-get autoclean
+
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\tGithub'da STAR icin Teşekkürlerimi sunarım...\n\t\t\tMehemmed (:)\n\n\n"
+
