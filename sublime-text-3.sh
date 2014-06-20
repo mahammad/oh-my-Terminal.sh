9488e5675d8a46c437e9ad0f89803b6545c92b80
msxiyev <msxiyev@gmail.com>
2014-06-20 17:01:04 +0300

    added sublime-text-3.sh

---
+++ sublime-text-3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer -y || -e
+echo
+echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\tGithub'da STAR icin Teşekkürlerimi sunarım...\n\t\t\tMehemmed (:)\n\n\n"
