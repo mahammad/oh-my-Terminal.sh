385a1653ed7eb2f059085753e9fadb1b62bb509b
msxiyev <msxiyev@gmail.com>
2014-06-28 00:21:40 +0300

    added How to install folder and move netbeans-8.sh that folder

--- netbeans-8.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get purge openjdk*
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-wget http://download.netbeans.org/netbeans/8.0/final/bundles/netbeans-8.0-linux.sh
-chmod +x netbeans-8.0-linux.sh
-./netbeans-8.0-linux.sh
