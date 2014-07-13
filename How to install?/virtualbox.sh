452bac34c5ee1d264561bc530b4b2ef0c77c91b1
msxiyev <msxiyev@gmail.com>
2014-07-13 17:46:04 +0300

    updated virtualbox.sh

--- How to install?/virtualbox.sh
+++ How to install?/virtualbox.sh
@@ -2,7 +2,6 @@
 apt-cache search virtualbox
 apt-cache show virtualbox
 sudo apt-get install virtualbox -y || -e
-echo
-echo "\n\n\tGithub'da STAR icin Teşekkürlerimi sunarım...\n\t\t\tMehemmed (:)"
+echo "\n\n\tThank you (:)"
 
 
