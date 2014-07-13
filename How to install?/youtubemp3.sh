4dd92415d5613bbc5d449c950ef980b4a1cacdc5
msxiyev <msxiyev@gmail.com>
2014-07-13 17:56:23 +0300

    updated youtubemp3.sh {add how to you work}

--- How to install?/youtubemp3.sh
+++ How to install?/youtubemp3.sh
@@ -1,3 +1,7 @@
+#1.Öncelikle dosyamıza çalışma izni verimemiz gerekiyor{ chmod +x youtubemp3.sh
+#2.dosyamızı /usr/bin/ dizinine istediğimiz isim altinda kopyaliyalim 
+#{sudo cp -rp youtubemp3.sh /usr/bin/mp3 (ben mp3 ismini verdim)
+#3. terminalden mp3 [youtubelink] indire bilirsiniz
 
 youtube-dl --extract-audio --audio-format mp3 $1 
 echo "\n\n\n\n\n\n\n\tSıradaki indirme... (:)"
