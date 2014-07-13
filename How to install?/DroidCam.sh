8374c5e06058671725bd891466057a6403e3a2be
msxiyev <msxiyev@gmail.com>
2014-07-13 17:17:43 +0300

    updated DroidCam.sh

--- How to install?/DroidCam.sh
+++ How to install?/DroidCam.sh
@@ -1,10 +1,15 @@
-#DroidCam – Use your Android phone as a wireless webcam or an IP Cam!
-#Install client app on your PC from my PPA with:
-
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Who you are <mail adress>
+# when this created and updated
+# OS requirements
+#
+# DroidCam
+# Use your Android phone as a wireless webcam or an IP Cam!
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 sudo apt-get install linux-headers-$(uname -r) build-essential -y || -e
 sudo apt-add-repository ppa:paolorotolo/droidcam
 sudo apt-get update
 sudo apt-get install droidcam -r || -e
 
-echo "\n\n\n\tDo not forget click the STAR,thank you (:)"
-echo "\n\n\n\tSTAR'a Tıklamayı unutmaın, teşekkürler (:)"
+echo "\n\n\n\tThank you (:)"
