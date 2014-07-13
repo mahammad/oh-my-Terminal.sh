b37221ef9b7e209137a09339ed9715cf3c59ede7
msxiyev <msxiyev@gmail.com>
2014-07-13 17:27:31 +0300

    updated  android-studio.sh

--- How to install?/android-studio.sh
+++ How to install?/android-studio.sh
@@ -3,4 +3,4 @@
 sudo add-apt-repository ppa:paolorotolo/android-studio
 sudo apt-get update
 sudo apt-get install android-studio -y || -e
-echo "\n\n\tGithub'da STAR icin Teşekkürlerimi sunarım...\n\t\t\tMehemmed (:)"
+echo "\n\n\t Thank you (:)"
