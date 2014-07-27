2262056dff54fbe31e2071518f41730c0733cd61
msxiyev <msxiyev@gmail.com>
2014-07-27 17:15:56 +0300

    added youtube-dlError.sh

---
+++ How to install?/youtube-dlError.sh
@@ -0,0 +1,5 @@
+#ERROR: no write permissions on /usr/bin/youtube-dl
+
+sudo wget https://raw.githubusercontent.com/rg3/youtube-dl/master/youtube-dl 
+sudo chmod +x youtube-dl
+sudo mv youtube-dl /usr/bin/youtube-dl
