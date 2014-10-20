9df792f8591202a1df59f51c8f66d46be2c41fd9
smehemmed <msxiyev@gmail.com>
2014-10-20 23:30:34 +0300

    update control triagle example

--- Bash programing example/control_triangle.sh
+++ Bash programing example/control_triangle.sh
@@ -1,11 +1,14 @@
- read X
- read Y
- read Z
+read X
+read Y
+read Z
+
 if [ $X -a $Y -a $Z -lt 1000 ]; then
 
 if [ $X -ne $Y -a $Y -ne $Z -a $X -ne $Z ]; then 
       echo "SCALENE"
 elif [ $X -eq $Y -a $Y -eq $Z -a $X -eq $Z ]; then
       echo "EQUILATERAL"
+else 
+    echo "ISOSCELES"
 fi
 fi
