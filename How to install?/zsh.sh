5c72af1be02f6ca71acbbef7eb86395f22789f46
smehemmed <msxiyev@gmail.com>
2014-10-30 01:28:04 +0200

    update fix

--- How to install?/zsh.sh
+++ How to install?/zsh.sh
@@ -1,9 +1,9 @@
+sudo apt-get install git -y || e
 wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 sudo apt-get install zsh
 cp ~/.zshrc ~/.zshrc.orig
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh
-
-echo "\n\n\tThank you (:)"
-
+echo
+echo "Thank you (:)"
